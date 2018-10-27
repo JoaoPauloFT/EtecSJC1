07b3c48c7de87930f84fe58a77554f143b2f1819
JoaoPauloFT <joaopaulotimoteo@gmail.com>
2018-10-27 12:36:30 -0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'etecsjc');
+define('DB_NAME', 'id7640624_etecsjc');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'id7640624_root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'SenhaSiteEtecSjc');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:3306');
+define('DB_HOST', 'https://databases.000webhost.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
