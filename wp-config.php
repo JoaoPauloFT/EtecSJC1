064d475f976ac6a902ae944895b47fcb92ca0663
JoaoPauloFT <joaopaulotimoteo@gmail.com>
2018-10-28 09:41:21 -0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'id7640624_root');
 define('DB_PASSWORD', 'SenhaSiteEtecSjc');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'webhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
